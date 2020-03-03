9078b90b36dae83b388a741036caabb84e19909c
Sal Tamay <sal.tamay@outlook.com>
2020-03-03 18:44:33 -0500

    feat: Add get/post routes to test the api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { listings } from './listings';
+
 const app = express();
 const PORT = 9000;
 
-const one = 1;
-const two = 2;
+app.use(bodyParser.json());
 
-app.get('/', (_req, res) => {
-  res.send(`1 + 2 = ${one + two}`);
+app.get('/listings', (_req, res) => {
+  return res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  listings.forEach((listing, idx) => {
+    if (listing.id === id) {
+      return res.send(listings.splice(idx, 1));
+    }
+  });
+
+  return res.send('failed to delete listing');
 });
 
 app.listen(PORT);
