573d63b913b2808c78ece9cd620d6c5c0a4e8c6f
Sal Tamay <sal.tamay@outlook.com>
2020-03-03 02:15:53 -0500

    feat: Node/Express server setup with Typescript and ESlint

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const PORT = 9000;
+
+const one = 1;
+const two = 2;
+
+app.get('/', (_req, res) => {
+  res.send(`1 + 2 = ${one + two}`);
+});
+
+app.listen(PORT);
+
+console.log(`[app]: http://localhost:${PORT}`);
