4dfdf561c6d4b3472d2079a977df7a3ef467ded4
Sal Tamay <sal.tamay@outlook.com>
2020-03-04 22:29:47 -0500

    feat: Add GraphQL and Apoolo Server

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const PORT = 9000;
 
-app.use(bodyParser.json());
-
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  listings.forEach((listing, idx) => {
-    if (listing.id === id) {
-      return res.send(listings.splice(idx, 1));
-    }
-  });
-
-  return res.send('failed to delete listing');
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(PORT);
 
